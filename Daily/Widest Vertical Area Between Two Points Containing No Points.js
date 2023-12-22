d398dec34ddfd1ea62bb176ecc148e3bdd3fbbee
amitojj <amitojj@gmail.com>
2023-12-22 16:47:03 +0530

    widest width

--- Daily/Widest Vertical Area Between Two Points Containing No Points.js
+++ Daily/Widest Vertical Area Between Two Points Containing No Points.js
@@ -17,14 +17,7 @@ Example 2:
 Input: points = [[3,1],[9,0],[1,0],[1,4],[5,3],[8,8]]
 Output: 3
  */
-var maxWidthOfVerticalArea = function (points) {
-  points.sort((a, b) => a[0] - b[0]);
-  let max = 0;
-  for (let i = 1; i < points.length; i++) {
-    max = Math.max(max, points[i][0] - points[i-1][0]);
-  }
-  return max;
-};
+;
 
 let points = [
   [3, 1],
